b3c932d68dc5a83e491ba1f66acee99d49cbd1c8
Esteban <esteban.alh@gmail.com>
2026-03-29 22:16:25 -0300

    Move DB credentials to environment variables
    Move DB credentials to environment variables

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
+import os
 from sqlalchemy import create_engine
 
-DB_URI = "mysql+pymysql://Lavatumaquina01:Lavatest2025@Lavatumaquina01.mysql.pythonanywhere-services.com/Lavatumaquina01$default"
+DB_URI = os.environ.get("DB_URI")
 engine = create_engine(DB_URI, pool_pre_ping=True)
 
 db_config = {
-     'host': 'Lavatumaquina01.mysql.pythonanywhere-services.com',
-     'user': 'Lavatumaquina01',
-     'password': 'Lavatest2025',
-     'database': 'Lavatumaquina01$default'
+    'host': os.environ.get("DB_HOST"),
+    'user': os.environ.get("DB_USER"),
+    'password': os.environ.get("DB_PASSWORD"),
+    'database': os.environ.get("DB_NAME")
 }
