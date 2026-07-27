d15a29bb76e4b6b8f2b280c09cec584044bb4a4e
Esteban <esteban.alh@gmail.com>
2026-07-27 08:53:08 -0400

    Integrate SQLAlchemy for database connection
    Added SQLAlchemy engine creation for database connection.

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
 import os
+from sqlalchemy import create_engine
+
+DB_URI = os.environ.get("DB_URI")
+engine = create_engine(DB_URI, pool_pre_ping=True)
 
 db_config = {
     'host': os.environ.get("DB_HOST"),
